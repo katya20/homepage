70e6497bd38b0fc7925031af0718cf9bef9011c1
katya20 <kpanchenko21@gmail.com>
2016-10-24 09:40:55 -0700

    Classadoo autocommit

--- page.js
+++ page.js
@@ -78,3 +78,35 @@ $("#clear-button-2").click(function() {
   deleteValue("list2")
   $("#output2").html("");
 });
+
+
+console.log("script.js is running");
+
+var name;
+var nameInput = $("#name-input")
+var noteInput = $("#note-input")
+var noteList = $("#note-list")
+
+nameInput.keydown(function(e) {
+  if (e.which == 13) {
+    getNotesForName(nameInput.val());
+    console.log(nameInput.val());
+    $("#note-list").html("");
+  }	
+})
+
+function getNotesForName(name) {  
+  onNewListItem(nameInput.val() + "/notes", function(note) {
+    $("#note-list").append("<div>" + note + "</div>")
+  })
+}
+
+noteInput.keydown(function(e) {
+  if (e.which == 13) {    
+    addListItem(nameInput.val() + "/notes", noteInput.val())
+    console.log("noteInput keydown is working");
+    $("#note-list").append(noteInput.val());
+  }
+})
+
+
